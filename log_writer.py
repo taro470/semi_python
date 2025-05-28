500a164b75d6ec546be0c1ab1341825d50705706
ryutaro <ryu703703703@gmail.com>
2025-05-28 11:28:38 +0900

    delete japanese part

--- log_writer.py
+++ log_writer.py
@@ -1,28 +1,23 @@
-import psutil     # リソースの情報を取得するためのライブラリ
-import datetime   # 現在時刻取得に使用するライブラリ
-import csv        # csvファイルのデータを読み書きするためのライブラリ
-import os         # ファイル操作に関するライブラリ
+import psutil     
+import datetime   
+import csv        
+import os         
 
-# データを格納するcsvファイル(system_log.csv)のパスを定義
-CSV_FILE = "/home/[①自分のユーザ名]/semi_python/system_log.csv"
+CSV_FILE = "/home/*1*/semi_python/system_log.csv"
 
-# 使用しているリソースのデータを取得しcsvファイルに記述する関数を定義
-[②] collect_data():
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # 現在時刻の取得
-    cpu = psutil.cpu_percent(interval=1)                        # CPU使用率の取得
-    mem = psutil.virtual_memory().percent                       # メモリ使用率の取得
-    disk = psutil.disk_usage('/').percent                       # ディスク使用率の取得
+*2* collect_data():
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+    cpu = psutil.cpu_percent(interval=1)                        
+    mem = psutil.virtual_memory().percent                       
+    disk = psutil.disk_usage('/').percent                       
 
-    # csvファイルが存在すれば1,そうでなければ0
-    file_exists = os.path.isfile([③])
+    file_exists = os.path.isfile(*3*)
                                  
-    # csvファイルを開いてデータの書き込み
-    with open([④], mode='a', newline='') as f:
+    with open(*4*, mode='a', newline='') as f:
         writer = csv.writer(f)
-        if not file_exists:  # パスにファイルが無かった場合、最初にヘッダ追加
+        if not file_exists:  
             writer.writerow(["datetime", "cpu", "memory", "disk"])
-        writer.writerow([⑤■■,■■,■■,■■])
+        writer.writerow([*5*])
 
-# ファイルが直接実行された場合にのみ、collect_data関数を実行
 if __name__ == "__main__":
-    [⑥関数の実行]
+    *6*
--- plot_graph.py
+++ plot_graph.py
@@ -1,18 +1,18 @@
-import pandas as pd                # ライブラリのインポート
-import matplotlib.pyplot as plt    # ライブラリのインポート
-import matplotlib.dates as mdates
-
-CSV_FILE = "/home/ryutaro/semi_python/system_log.csv"
-IMG_FILE = "/home/ryutaro/semi_python/system_graph.png"
+*1* pandas as pd                
+*1* matplotlib.pyplot as plt    
+*1* matplotlib.dates as mdates  
+ 
+CSV_FILE = "/home/*2*/semi_python/system_log.csv"
+IMG_FILE = "/home/*2*/semi_python/system_graph.png"
 
 def plot_graph():
     df = pd.read_csv(CSV_FILE)
     df["datetime"] = pd.to_datetime(df["datetime"])
 
     plt.figure(figsize=(10, 5))
-    plt.plot(df["datetime"], df["cpu"], label="CPU (%)", color="red")
-    plt.plot(df["datetime"], df["memory"], label="Memory (%)", color="blue")
-    plt.plot(df["datetime"], df["disk"], label="Disk (%)", color="green")
+    plt.plot(df["datetime"], df[*3*], label="CPU (%)", color="red")
+    plt.plot(df["datetime"], df[*4*], label="Memory (%)", color="blue")
+    plt.plot(df["datetime"], df[*5*], label="Disk (%)", color="green")
 
     plt.xlabel("Time")
     plt.ylabel("Usage (%)")
@@ -23,8 +23,8 @@ def plot_graph():
     plt.savefig(IMG_FILE)
     plt.close()
 
-    # x軸を秒を除いたフォーマットに設定
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
 
+
 if __name__ == "__main__":
-    plot_graph()
+    *6*

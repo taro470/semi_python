02aeef9b0f5235026527796f51f9596222accd7e
ryutaro <ryu703703703@gmail.com>
2025-05-28 11:09:22 +0900

    file changed

--- log_writer.py
+++ log_writer.py
@@ -1,22 +1,28 @@
-import psutil
-import datetime
-import csv
-import os
+import psutil     # リソースの情報を取得するためのライブラリ
+import datetime   # 現在時刻取得に使用するライブラリ
+import csv        # csvファイルのデータを読み書きするためのライブラリ
+import os         # ファイル操作に関するライブラリ
 
-CSV_FILE = "/home/ryutaro/semi_python/system_log.csv"
+# データを格納するcsvファイル(system_log.csv)のパスを定義
+CSV_FILE = "/home/[①自分のユーザ名]/semi_python/system_log.csv"
 
-def collect_data():
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    cpu = psutil.cpu_percent(interval=1)
-    mem = psutil.virtual_memory().percent
-    disk = psutil.disk_usage('/').percent
+# 使用しているリソースのデータを取得しcsvファイルに記述する関数を定義
+[②] collect_data():
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # 現在時刻の取得
+    cpu = psutil.cpu_percent(interval=1)                        # CPU使用率の取得
+    mem = psutil.virtual_memory().percent                       # メモリ使用率の取得
+    disk = psutil.disk_usage('/').percent                       # ディスク使用率の取得
 
-    file_exists = os.path.isfile(CSV_FILE)
-    with open(CSV_FILE, mode='a', newline='') as f:
+    # csvファイルが存在すれば1,そうでなければ0
+    file_exists = os.path.isfile([③])
+                                 
+    # csvファイルを開いてデータの書き込み
+    with open([④], mode='a', newline='') as f:
         writer = csv.writer(f)
-        if not file_exists:
+        if not file_exists:  # パスにファイルが無かった場合、最初にヘッダ追加
             writer.writerow(["datetime", "cpu", "memory", "disk"])
-        writer.writerow([now, cpu, mem, disk])
+        writer.writerow([⑤■■,■■,■■,■■])
 
+# ファイルが直接実行された場合にのみ、collect_data関数を実行
 if __name__ == "__main__":
-    collect_data()
+    [⑥関数の実行]
